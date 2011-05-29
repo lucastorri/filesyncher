c4bd740e632f9734379177fd801a113edeecdc1d
Lucas Torri <lucastorri@gmail.com>
2011-05-29 20:57:41 -0300

    adding idea and akka plugin

--- project/build/FileSyncherProject.scala
+++ project/build/FileSyncherProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class FileSyncherProject(info: ProjectInfo) extends DefaultProject(info) {
+class FileSyncherProject(info: ProjectInfo) extends DefaultProject(info) with AkkaProject with IdeaProject {
 
   val scalaTest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test"
   val mockito   = "org.mockito" % "mockito-all" % "1.8.5" % "test"
---
+++ project/plugins/Plugins.scala
@@ -0,0 +1,10 @@
+import sbt._
+
+class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val akkaRepo = "Akka Repo" at "http://akka.io/repository"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1.2"
+
+  val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+  val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
+}
+
